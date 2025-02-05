2f9a49218a71ba1ac70f75e72ecc9c299cbc0ab2
razenry <razenry.code@gmail.com>
2025-02-05 10:19:44 +0700

    Alhandulillah

--- index.php
+++ index.php
@@ -1,12 +1,10 @@
 <?php 
 session_start();
 $_SESSION['app_name'] = 'Washa'; 
-// Include the initialization file which sets up necessary configurations and dependencies
+
 require_once 'init.php';
 
-// Set the timezone to Indonesian standard time using the DateHelper class
 DateHelper::setIndonesianTimeZone();
 DB::connect('localhost', 'washa', 'root', '');
 
-// Instantiate the main application class
 $app = new App();
